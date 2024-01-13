4e1942487dcfda7d07664895c25c48a47ccd1435
Artyom <artyom-first@yandex.ru>
2024-01-13 22:01:00 +0500

    feat(Banner): add the Banner module

---
+++ src/modules/Banner/Banner.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './Banner.scss';
+
+const Banner: FC = () => {
+  return (
+    <section className='banner'>
+      <h1 className='banner__title'>База отдыха &laquo;У семи озёр&raquo;</h1>
+    </section>
+  );
+};
+
+export default Banner;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './MainPage.css';
+import React, { FC } from 'react';
+import './MainPage.scss';
+import Banner from '../../modules/Banner/Banner';
 
-const MainPage = () => {
+const MainPage: FC = () => {
   return (
     <main className='main-page'>
-      
+      <Banner />
     </main>
   );
 };
