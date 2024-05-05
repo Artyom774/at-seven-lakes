ad95b92349edc9d74cc99e652025cab1ee56e6f8
Artyom <artyom-first@yandex.ru>
2024-05-05 16:41:46 +0500

    refactor(App): replace texts for TextSection in data

--- src/pages/PlanPage/PlanPage.tsx
+++ src/pages/PlanPage/PlanPage.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import PageTitleSection from '../../modules/PageTitleSection/PageTitleSection';
 import BasePlan from '../../modules/BasePlan/BasePlan';
 import TextSection from '../../modules/TextSection/TextSection';
+import { planPageText } from '../../data/pageTexts';
 import pageIcon from '../../images/plan-page-icon.jpg';
 
 interface PlanPageProps {
@@ -9,22 +10,11 @@ interface PlanPageProps {
 }
 
 const PlanPage: FC<PlanPageProps> = ({ title }) => {
-  const textArray: string[] = [
-    'Заезд с 16:00, выезд до 14:00 часов.',
-    'Проживание с домашними питомцами возможно при согласовании с администрацией по телефону.',
-    '',
-    'Услуга бронирования - безвозвратная.',
-    '',
-    'Парковка автомобилей бесплатна и возможна только на парковочной зоне.',
-    '',
-    'Мангалы и дрова входят в стоимость аренды домика.',
-  ];
-
   return (
     <main className='main'>
       <PageTitleSection image={pageIcon} title={title} subtitle='План базы' />
       <BasePlan />
-      <TextSection title='Важно!' textArray={textArray} />
+      <TextSection title='Важно!' text={planPageText} />
     </main>
   );
 };
