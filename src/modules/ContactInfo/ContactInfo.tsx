9bf0a8fac3aba21b4f4736dc544fa95f47d79ced
Artyom <artyom-first@yandex.ru>
2024-05-09 10:09:28 +0500

    feat(ContactInfo): сверстает модуль

---
+++ src/modules/ContactInfo/ContactInfo.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import './ContactInfo.scss';
+import { phone } from '../../utils/constants';
+import LocationIcon from '../../images/icons/location.svg';
+import MobileIcon from '../../images/icons/mobile.svg';
+
+const ContactInfo: FC = () => {
+  return (
+    <section className='contact-info'>
+      <div className='contact-info__point'>
+        <img className='contact-info__icon' src={LocationIcon} alt='Местоположение' />
+        <span className='contact-info__text'>Челябинская бласть, Каслинский район, У семи озёр</span>
+      </div>
+      <div className='contact-info__point'>
+        <span className='contact-info__text'>{phone}</span>
+      </div>
+      <div className='contact-info__point'>
+        <img className='contact-info__icon' src={MobileIcon} alt='Сотовый телефон' />
+        <span className='contact-info__text'>Вы можете позвонить нам по телефону или через WhatsApp</span>
+      </div>
+    </section>
+  );
+};
+
+export default ContactInfo;
