6b05a2e1ce8488760c4f1b5dcec788d9c6aab53f
Artyom <artyom-first@yandex.ru>
2024-06-03 21:00:23 +0500

    feat(HoldingEventPage): add photos

---
+++ src/data/holdingEventPage/index.ts
@@ -0,0 +1,19 @@
+import photo1 from './he01.jpg';
+import photo2 from './he02.jpg';
+import photo3 from './he03.jpg';
+import photo4 from './he04.jpg';
+import photo5 from './he05.jpg';
+import photo6 from './he06.jpg';
+import photo7 from './he07.jpg';
+
+const holdingEventsImages: string[] = [
+  photo1,
+  photo2,
+  photo3,
+  photo4,
+  photo5,
+  photo6,
+  photo7,
+];
+
+export default holdingEventsImages;
