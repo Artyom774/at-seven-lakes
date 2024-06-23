a531ce150ba2d6887272210a4ee58934ddfe1252
Artyom <artyom-first@yandex.ru>
2024-06-23 20:38:44 +0500

    feat(photos): заменит и добавит фотографии

--- src/data/houses/16/index.ts
+++ src/data/houses/16/index.ts
@@ -1,11 +1,7 @@
 import photo1 from './16_01.jpg';
-import photo2 from './16_02.jpg';
-import photo3 from './16_03.jpg';
 
 const images16: string[] = [
   photo1,
-  photo2,
-  photo3,
 ];
 
 export default images16;
