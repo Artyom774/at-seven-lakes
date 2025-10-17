74f71c71dcbbf8a7820d71f59ec64aa0813feb4f
Artyom <artyom-first@yandex.ru>
2025-10-17 11:45:39 +0500

    feat(App): обновит тексты и карту

--- src/modules/Banner/Banner.tsx
+++ src/modules/Banner/Banner.tsx
@@ -4,7 +4,7 @@ import './Banner.scss';
 const Banner: FC = () => {
   return (
     <section className='banner'>
-      <h1 className='banner__title'>Клуб &laquo;Карась&raquo;</h1>
+      <h1 className='banner__title'>ЗАГОРОДНЫЙ КЛУБ КАРАСЬ</h1>
     </section>
   );
 };
