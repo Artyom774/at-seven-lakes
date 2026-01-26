17bf70c8a61d5e6ae24f5e9564ac4a2b03457f3d
Artyom <artyom-first@yandex.ru>
2026-01-26 09:50:12 +0500

    feat(housesData): обновит фотографии для домиков 13а и 13б

--- src/data/houses/13b/index.ts
+++ src/data/houses/13b/index.ts
@@ -4,13 +4,6 @@ import photo3 from './13b_03.jpg';
 import photo4 from './13b_04.jpg';
 import photo5 from './13b_05.jpg';
 import photo6 from './13b_06.jpg';
-import photo7 from './13b_07.jpg';
-import photo8 from './13b_08.jpg';
-import photo9 from './13b_09.jpg';
-import photo10 from './13b_10.jpg';
-import photo11 from './13b_11.jpg';
-import photo12 from './13b_12.jpg';
-import photo13 from './13b_13.jpg';
 
 const images13b: string[] = [
   photo1,
@@ -19,13 +12,6 @@ const images13b: string[] = [
   photo4,
   photo5,
   photo6,
-  photo7,
-  photo8,
-  photo9,
-  photo10,
-  photo11,
-  photo12,
-  photo13,
 ];
 
 export default images13b;
